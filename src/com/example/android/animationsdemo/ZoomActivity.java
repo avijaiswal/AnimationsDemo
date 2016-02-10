65a576daf7ba68e7d06e6e5d16a8717b827a75f9
avinash jaiswal <jaisavi25@gmail.com>
2016-02-10 12:00:11 +0530

    com

--- src/com/example/android/animationsdemo/ZoomActivity.java
+++ src/com/example/android/animationsdemo/ZoomActivity.java
@@ -30,6 +30,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.ImageView;
+
+/**
+ * A sample showing how to zoom an image thumbnail to full-screen, by animating the bounds of the
+ * zoomed image from the thumbnail bounds to the screen bounds.
+ *
+ * <p>In this sample, the user can touch one of two images. Touching an image zooms it in, covering
+ * the entire activity content area. Touching the zoomed-in image hides it.</p>
+ */
 public class ZoomActivity extends FragmentActivity {
     /**
      * Hold a reference to the current animator, so that it can be canceled mid-way.
@@ -82,6 +90,22 @@ public class ZoomActivity extends FragmentActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * "Zooms" in a thumbnail view by assigning the high resolution image to a hidden "zoomed-in"
+     * image view and animating its bounds to fit the entire activity content area. More
+     * specifically:
+     *
+     * <ol>
+     *   <li>Assign the high-res image to the hidden "zoomed-in" (expanded) image view.</li>
+     *   <li>Calculate the starting and ending bounds for the expanded view.</li>
+     *   <li>Animate each of four positioning/sizing properties (X, Y, SCALE_X, SCALE_Y)
+     *       simultaneously, from the starting bounds to the ending bounds.</li>
+     *   <li>Zoom back out by running the reverse animation on click.</li>
+     * </ol>
+     *
+     * @param thumbView  The thumbnail view to zoom in.
+     * @param imageResId The high-resolution version of the image represented by the thumbnail.
+     */
     private void zoomImageFromThumb(final View thumbView, int imageResId) {
         // If there's an animation in progress, cancel it immediately and proceed with this one.
         if (mCurrentAnimator != null) {
